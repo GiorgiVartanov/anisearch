56e48ee3dfe9c0156f4f53cdc605b932940fa37c
PhYell <94902895+PhYell@users.noreply.github.com>
2022-07-26 22:25:46 +0400

    changed a lot of things, it works same as before but better

--- src/components/body/anime-cards/AnimeCards.jsx
+++ src/components/body/anime-cards/AnimeCards.jsx
@@ -1,21 +1,92 @@
 import "./animeCards.scss";
 
 import AnimeCard from "../anime-card/AnimeCard.jsx";
-import PageSelect from "../../utility/pageSelect.component/PageSelect";
 
+import { useLazyQuery, useQuery } from "@apollo/client";
 import { useState, useEffect, useRef, useCallback } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
-import { Link } from "react-router-dom";
+import { client } from "../../../App";
 
-const AnimeCards = ({ data, myref }) => {
+import { GET_ANIME_LIST } from "../../../queries/animeQueries";
+
+const AnimeCards = ({ search }) => {
+    const [getNew, { loading, error, data, fetchMore }] =
+        useLazyQuery(GET_ANIME_LIST);
+    // const { loading, error, data, fetchMore } = useQuery(GET_ANIME_LIST, {
+    //     variables: { page: 1, perPage: 10, search: search },
+    // });
+
+    let page = 1; // I am not sure if it is right to use something like this in react
+
+    const observer = useRef();
+    const lastCard = useCallback(
+        // it is used to fetch more data when user is on the bottom of a page
+        (node) => {
+            // node is last created AnimeCard on page
+            if (loading) return;
+            if (observer.current) observer.current.disconnect(); // it will disconnect observer from a previous element
+            observer.current = new IntersectionObserver((entries) => {
+                if (
+                    entries[0].isIntersecting &&
+                    data.Page.pageInfo.hasNextPage
+                ) {
+                    fetchMore({
+                        variables: {
+                            page: page + 1,
+                        },
+                        updateQuery: (
+                            previousResult,
+                            { fetchMoreResult, queryVariables }
+                        ) => {
+                            page++;
+                            return fetchMoreResult;
+                        },
+                    });
+                }
+            });
+            if (node) observer.current.observe(node);
+        },
+        [loading]
+    );
+
+    useEffect(() => {
+        console.log("AnimeCards : ", search);
+        client.resetStore();
+        if (search === "")
+            getNew({
+                variables: { page: 1, perPage: 10 },
+                updateQuery: (
+                    previousResult,
+                    { fetchMoreResult, queryVariables }
+                ) => {
+                    return fetchMoreResult;
+                },
+            });
+        else
+            getNew({
+                variables: { page: 1, perPage: 10, search: search },
+                updateQuery: (
+                    previousResult,
+                    { fetchMoreResult, queryVariables }
+                ) => {
+                    return fetchMoreResult;
+                },
+            });
+    }, [search]);
+
+    if (!data) return <p>No Data</p>;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Something Went Wrong</p>;
+
     return (
         <div className="changed-centered">
             <div className="item-holder">
-                {data.map((show, index) => {
-                    if (data.length === index + 1) {
+                {data.Page.media.map((show, index) => {
+                    if (data.Page.media.length === index + 1) {
                         return (
                             <AnimeCard
-                                myref={myref}
+                                myref={lastCard}
                                 key={show.id}
                                 id={show.id}
                                 title={show.title.romaji}
--- src/pages/anime-list.page/AnimeList.page.jsx
+++ src/pages/anime-list.page/AnimeList.page.jsx
@@ -1,95 +1,42 @@
 import AnimeCards from "../../components/body/anime-cards/AnimeCards";
 
-import { useQuery } from "@apollo/client";
-import { useState, useEffect, useRef, useCallback } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-
-import { client } from "../../App";
-
-import { GET_ANIME_LIST } from "../../queries/animeQueries";
+import { useState, useEffect } from "react";
 
 const AnimeList = () => {
-    const { search } = useParams();
-    const navigate = useNavigate();
-
-    const [currentPage, setCurrentPage] = useState(1);
     const [searchedValue, setSearchedValue] = useState("");
     const [animeToSearch, setAnimeToSearch] = useState(null);
-
-    const { loading, error, data, fetchMore } = useQuery(GET_ANIME_LIST, {
-        variables: { page: 1, perPage: 10, search: search },
-    });
-
-    let page = 1; // I am not sure if it is right to use something like this in react
-
-    const observer = useRef();
-    const lastAnime = useCallback(
-        // it is used to fetch more data when user is on the bottom of a page
-        (node) => {
-            // node is created AnimeCard on page
-            if (loading) return;
-            if (observer.current) observer.current.disconnect(); // it will disconnect observer from a previous element
-            observer.current = new IntersectionObserver((entries) => {
-                if (
-                    entries[0].isIntersecting &&
-                    data.Page.pageInfo.hasNextPage
-                ) {
-                    fetchMore({
-                        variables: {
-                            page: page + 1,
-                        },
-                        updateQuery: (
-                            previousResult,
-                            { fetchMoreResult, queryVariables }
-                        ) => {
-                            page++;
-                            return fetchMoreResult;
-                        },
-                    });
-                }
-            });
-            if (node) observer.current.observe(node);
-        },
-        [loading]
-    );
 
     const handleSearch = (e) => {
         // MAKE IT SO IT NAVIGATES TO /:search
         e.preventDefault();
-        client.clearStore();
+        // client.clearStore();
         setSearchedValue(e.target.value);
-        setCurrentPage(1);
     };
 
     useEffect(() => {
         const timer = setTimeout(() => {
-            if (searchedValue === "") setAnimeToSearch(null);
-            else setAnimeToSearch(searchedValue);
-            page = 1;
+            setAnimeToSearch(searchedValue);
         }, 1500);
 
         return () => clearTimeout(timer);
     }, [searchedValue]);
 
-    useEffect(() => {
-        // console.log(animeToSearch);
-        if (animeToSearch === null) navigate(`/animelist`);
-        else navigate(`/animelist/${animeToSearch}`);
-    }, [animeToSearch]);
+    // useEffect(() => {
+    //     console.log(animeToSearch);
+    //     // if (animeToSearch === null) navigate(`/animelist`);
+    //     // else navigate(`/animelist/${animeToSearch}`);
+    // }, [animeToSearch]);
 
-    useEffect(() => {
-        console.log("search", search);
-        if (typeof search === "undefined")
-            console.log("now we need to search for ''");
-    }, [search]);
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Something Went Wrong</p>;
+    // useEffect(() => {
+    //     console.log("search", search);
+    //     if (typeof search === "undefined")
+    //         console.log("now we need to search for ''");
+    // }, [search]);
 
     return (
         <>
             <input type="text" onChange={handleSearch} value={searchedValue} />
-            <AnimeCards data={data.Page.media} myref={lastAnime} />
+            <AnimeCards search={animeToSearch} />;
         </>
     );
 };
