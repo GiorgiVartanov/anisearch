203bcc943d7734b29fadda56459dfb57e5fc8f55
PhYell <94902895+PhYell@users.noreply.github.com>
2022-08-02 22:15:22 +0400

    fixed error

--- src/queries/animeQueries.js
+++ src/queries/animeQueries.js
@@ -47,9 +47,30 @@ const GET_ANIME_LIST = gql`
     }
 `;
 
-const GET_ANIME_CARD = gql`
-    query getAnimeCard($id: Int) {
-        Media(id: $id, type: ANIME) {
+const GET_SEARCHED_ANIME = gql`
+    query getSearchedAnime(
+        $search: String
+        $startDate_greater: FuzzyDateInt
+        $startDate_lesser: FuzzyDateInt
+        $endDate_greater: FuzzyDateInt
+        $endDate_lesser: FuzzyDateInt
+        $format_in: [MediaFormat]
+        $episodes_greater: Int
+        $episodes_lesser: Int
+        $genre_in: [String]
+    ) {
+        Media(
+            type: ANIME
+            search: $search
+            startDate_greater: $startDate_greater
+            startDate_lesser: $startDate_lesser
+            endDate_greater: $endDate_greater
+            endDate_lesser: $endDate_lesser
+            format_in: $format_in
+            episodes_greater: $episodes_greater
+            episodes_lesser: $episodes_lesser
+            genre_in: $genre_in
+        ) {
             id
             title {
                 romaji
